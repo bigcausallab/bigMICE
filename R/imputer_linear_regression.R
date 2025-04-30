1952969cc2d015521b82aa29a496e7e1476ade5b
Hugo Morvan <Hugo.Morvan@coyotes.usd.edu>
2025-04-30 16:11:32 +0200

    .

--- R/imputer_linear_regression.R
+++ R/imputer_linear_regression.R
@@ -45,10 +45,12 @@ impute_with_linear_regression <- function(sc, sdf, target_col, feature_cols, ela
   lm_model <- complete_data %>%
     sparklyr::ml_linear_regression(formula = formula_obj,
                          elastic_net_param = elastic_net_param)
+  print(lm_model)
   print("7")
   #cat('class lm_model',class(lm_model),'\n')
   # Step 5: Predict missing values
   predictions <- sparklyr::ml_predict(lm_model, incomplete_data) %>% sparklyr::sdf_with_sequential_id("pred_id")
+  print(predictions)
   print("77")
   pred_residuals <- predictions %>%
     sparklyr::inner_join(target_col_prev, by = "id")
