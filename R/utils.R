e6ba8c33a4caa77a1fb4c78f5de1d89d58aff98e
Hugo Morvan <Hugo.Morvan@coyotes.usd.edu>
2025-08-20 11:46:05 +0200

    Added 2 utility functions to facilitate predictor matrix specification

--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,51 @@ get_named_vector <- function(csv_file) {
   ordered_vector <- named_vector[order(names(named_vector))] #Order alphabet.
   return(ordered_vector)
 }
+
+#' Create Predictor Matrix csv
+#'
+#' Utility function to create a matrix or a csv File containing the variables names x variables names matrix to be used for specifying the predictor matrix to be used during the bigMice procedure. The csv file can then be opened with a spreadsheet software (e.g. Excel, Google sheets, etc.) to visualize and specify the predictors of each variable more easily and in a friendlier interface.
+#'
+#' @param df The dataframe, including the column names.
+#' @param save_file Default value is NULL. If a save file is specified, the function will save the created matrix as a .csv file with the name and location specified.
+#' @return A predictor matrix with default settings (all predictors used) for the bigMice imputation procedure.
+#' @export
+#' @examples
+#' #TBD
+create_predictor_matrix <- function(df, save_file=NULL){
+  # Utility function to create a predictor matrix for bigMICE
+  # takes a R dataframe and saves a excel file with the default predictor matrix
+  # optional argument save to also save the matrix to an csv file to be modified (easier)
+
+  # Extract variable names
+  variables <- colnames(df)
+
+  # Create a TRUE matrix with FALSE on the diagonal
+  predictor_matrix <- matrix(TRUE, nrow = length(variables), ncol = length(variables),
+                             dimnames = list(variables, variables))
+  diag(predictor_matrix) <- FALSE  # Set diagonal to FALSE
+
+  # save the matrix to an excel file
+  if (save){
+    write.csv(as.data.frame(predictor_matrix), "predictor_matrix.csv")
+  }
+  return(predictor_matrix)
+}
+
+
+#' Import Predictor Matrix csv
+#'
+#' Utility function to import back a predictor matrix from a csv file created by the create_predictor_matrix() function.
+#'
+#' @param file The csv file to be opened
+#' @return A predictor matrix with specified settings (predictors used) for the bigMice imputation procedure.
+#' @export
+#' @examples
+#' #TBD
+#'
+import_predictor_matrix <- function(file){
+  # Utility function to import a predictor matrix from an csv file
+  # takes a file path and returns the matrix as a matrix object
+  data <- read.csv(file, row.names = 1)
+  return(as.matrix(data))
+}
